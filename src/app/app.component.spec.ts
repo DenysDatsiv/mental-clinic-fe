6b70c6adaa07bce6da641bd486e9cd482843428e
DenysDatsiv <dendatsiv@gmail.com>
2025-02-18 17:43:02 +0200

    fix deploy config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mental-front-end'`, () => {
+  it(`should have as title 'mental-clinic-fe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mental-front-end');
+    expect(app.title).toEqual('mental-clinic-fe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mental-front-end');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mental-clinic-fe');
   });
 });
