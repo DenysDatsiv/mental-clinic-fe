8bfd6550bb69ba9ab998f34c82d4fcb5d61f087f
Denys Datsiv <dendatsiv@gmail.com>
2026-03-10 14:34:11 +0200

    rename

--- src/app/masterfile/shared/constants/components-name.constants.ts
+++ src/app/masterfile/shared/constants/components-name.constants.ts
@@ -1,7 +1,7 @@
 export const TELEGRAM_BANNER ={
 
-    PSYCHOLOGISTS_COMMUNITY_LABEL : {name:'Спільнота психологів',link:'https://t.me/+arTvl6C3HeQ2ZjVi'},
-    DOCTORS_COMMUNITY_LABEL : {name:'Спільнота лікарів',link:'https://t.me/+EzLZPYezLm03NTNi'}
+    PSYCHOLOGISTS_COMMUNITY_LABEL : {name:'Чат-підтримки ( відповіді психіатрів ) ',link:'https://t.me/+arTvl6C3HeQ2ZjVi'},
+    DOCTORS_COMMUNITY_LABEL : {name:'Чат-підтримки ( відповіді психологів )',link:'https://t.me/+EzLZPYezLm03NTNi'}
 }
 
 
